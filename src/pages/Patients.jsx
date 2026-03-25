8507fd3ecd869cddbfb3d84ad244af95c54d485d
Grish Maharjan <grishmaharjan9@gmail.com>
2026-03-25 21:24:07 +0545

    Healthcare

---
+++ src/pages/Patients.jsx
@@ -0,0 +1,9 @@
+function Patients(){
+    return(
+        <div className = "p-4">
+            <h1 className = "text-3xl font-bold text-grya-900">Name of the Patients</h1>
+        </div>
+
+    )
+}
+export default Patients
