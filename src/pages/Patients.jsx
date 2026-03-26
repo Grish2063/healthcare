5fa7fc060d08d593afd4a080df54a7f9ecb080e0
Grish Maharjan <grishmaharjan9@gmail.com>
2026-03-26 21:04:10 +0545

    Healthcare

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -3,6 +3,7 @@ function Patients(){
         <div className = "p-4">
             <h1 className = "text-3xl font-bold text-gray-900">Name of the Patients</h1>
             <p1 className = "text-3xl font-bold text-gray-900">Joe</p1>
+             <p1 className = "text-3xl font-bold text-gray-900">John</p1>
         </div>
 
     )
