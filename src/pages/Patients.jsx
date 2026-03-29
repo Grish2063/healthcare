d3c00b4e638a0b11f9359dfcafccdf50303d353f
Grish Maharjan <grishmaharjan9@gmail.com>
2026-03-29 19:19:46 +0545

    Healthcare

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -6,6 +6,7 @@ function Patients() {
       <p className ="text-gray-600 mt-4">John</p>
       <p className ="text-gray-600 mt-4">Jenny</p>
       <p className = "text-blue-600 mt-4">Kai</p>
+      <p className = "text-blue-600 mt-4">Alex</p>
 
     </div>
   );
