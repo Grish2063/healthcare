5beb3df65a1296a9fe891bf12826f78d5a23083f
Grish Maharjan <grishmaharjan9@gmail.com>
2026-04-19 21:29:06 +0545

    Heathcare

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -2,6 +2,9 @@ function Patients() {
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold text-gray-900">Name of the Patients</h1>
+      <p className="text-gray-600 mt-4">Ram</p>
+      <p className="text-gray-600 mt-4">Joe</p>
+      
 
     </div>
   );
