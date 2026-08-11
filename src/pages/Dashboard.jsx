2a51c070cd0e6246e080a863929bfa4c7585ba5b
Grish Maharjan <grishmaharjan9@gmail.com>
2026-08-11 20:13:47 +0545

    Healthcare

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -293,7 +293,6 @@ function Dashboard() {
             />
             <QuickActionButton
               label="Upload Records"
-              disabled
               iconPath="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
               colorClass={{ bg: 'bg-purple-50', text: 'text-purple-600', hoverBg: 'hover:bg-purple-100', hoverBorder: 'border-purple-500' }}
             />
